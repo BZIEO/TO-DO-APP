f9f39fc3d2df62ba8f54f6a12f506ccf2ee4eaa1
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-03-31 12:25:59 -0500

    Added delete button for list item

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let ul = document.querySelector(".ul");
 
 btn.addEventListener("click", function () {
   let list = document.createElement("li");
+  let delBtn = document.createElement("button");
+  delBtn.innerHTML = "Remove";
   list.innerHTML = txt.value;
   ul.appendChild(list);
+  list.appendChild(delBtn);
+
+  delBtn.addEventListener("click", function () {
+    ul.removeChild(list);
+  });
 });
